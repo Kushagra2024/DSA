22ec8bbe29b06134dc113c737534677006652c1c
Kushagra2024 <Kushagrawal202@gmail.com>
2023-02-04 23:29:22 +0530

    Arrays(2D) Added

---
+++ DATA STRUCTURES/ARRAYS/ARRAY_2D.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+    /*-----------------------------------------Two-dimensional Arrays-----------------------------------------*/
+    //Declaring Two-dimensional Arrays
+    int marks_1[3][5];
+
+    //Initializing Two-dimensional Arrays
+    int marks_2[2][3] = {1,2,3,4,5,6};
+    int marks_3[2][3] = {{1,2,3},{4,5,6}};
+    int marks_4[][3] = {1,2,3,4,5,6};
+
+    return 0;
+}
