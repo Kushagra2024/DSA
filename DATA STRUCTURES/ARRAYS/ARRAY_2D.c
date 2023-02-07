af6f3ed16e33efbffbaf6985e81d267860b22894
Kushagra2024 <Kushagrawal202@gmail.com>
2023-02-07 23:19:35 +0530

    Arrays(2D) Updated

--- DATA STRUCTURES/ARRAYS/ARRAY_2D.c
+++ DATA STRUCTURES/ARRAYS/ARRAY_2D.c
@@ -11,5 +11,16 @@ int main()
     int marks_3[2][3] = {{1,2,3},{4,5,6}};
     int marks_4[][3] = {1,2,3,4,5,6};
 
+    //Accessing the Elements of Two-dimensional Arrays
+    int i, j;
+    for (i = 0; i < 2; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            printf("%d\t",marks_4[i][j]);
+        }
+        printf("\n");
+    }
+    
     return 0;
 }
